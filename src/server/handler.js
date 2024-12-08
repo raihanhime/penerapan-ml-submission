061e66391d037f3167f50b2afce5ea41befa7fb8
raihanhime <114859494+raihanhime@users.noreply.github.com>
2024-12-08 13:42:38 +0700

    Update handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,54 +1,55 @@
 const predictClassification = require('../services/inferenceService');
-const { v4: uuidv4 } = require('uuid');
+const crypto = require('crypto');
 const storeData = require('../services/storeData');
 const getAllData = require('../services/getAllData');
-
+ 
 async function postPredictHandler(request, h) {
   const { image } = request.payload;
   const { model } = request.server.app;
-
+ 
   const { label, suggestion } = await predictClassification(model, image);
-  const id = uuidv4();
+  const id = crypto.randomUUID();
   const createdAt = new Date().toISOString();
-
+ 
   const data = {
-    id,
-    result: label,
-    suggestion,
-    createdAt
-  };
-
+    "id": id,
+    "result": label,
+    "suggestion": suggestion,
+    "createdAt": createdAt
+  }
+ 
   await storeData(id, data);
 
   const response = h.response({
     status: 'success',
     message: 'Model is predicted successfully',
     data
-  });
+  })
   response.code(201);
   return response;
 }
-
+ 
 async function postPredictHistoriesHandler(request, h) {
   const allData = await getAllData();
-
-  const formatAllData = allData.map(doc => {
-    const data = doc.data();
-    return {
-      id: doc.id,
-      history: {
-        result: data.result,
-        createdAt: data.createdAt,
-        suggestion: data.suggestion,
-        id: doc.id
-      }
-    };
+  
+  const formatAllData = [];
+  allData.forEach(doc => {
+      const data = doc.data();
+      formatAllData.push({
+          id: doc.id,
+          history: {
+              result: data.result,
+              createdAt: data.createdAt,
+              suggestion: data.suggestion,
+              id: doc.id
+          }
+      });
   });
-
+  
   const response = h.response({
     status: 'success',
     data: formatAllData
-  });
+  })
   response.code(200);
   return response;
 }
